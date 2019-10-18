f69b5e5fbf8433827767b7ff49b22533f3273fa1
meemeee <lethitramy27@gmail.com>
2019-10-18 21:01:22 +0900

    add channels.html

--- application.py
+++ application.py
@@ -7,11 +7,22 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# Set global variables
+channel_list = list()
 
 @app.route("/")
 def index():
     return "Project 2: TODO"
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/channels")
+def channels():
+    """ Display all channels """
+
+    return render_template("channels.html", channels=channel_list)
+
+if __name__ == '__main__':
+    socketio.run(app)
